e88631e7664077829e22bda10993b12b2841741b
jlopezhonduras <jorge.lopezhn@gmail.com>
2025-02-12 13:30:17 -0600

    1

--- libs/scripts.js
+++ libs/scripts.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+
+const urlJSON = 'https://raw.githubusercontent.com/jlopezhonduras/Honduras-en-Cifras/master/datos/datos.json';
+        
+$.get(urlJSON)
+    .done(data => {
+        $("#contenido").html(`
+            <h2>${data.pais}</h2>
+            <p>Población: ${data.poblacion.toLocaleString()}</p>
+            <p>Municipios: ${data.municipios.join(", ")}</p>
+        `);
+    })
+    .fail(error => {
+        console.error("Error:", error);
+        $("#contenido").html("Error al cargar datos.");
+    });
