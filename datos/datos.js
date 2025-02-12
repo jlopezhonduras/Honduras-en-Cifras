345a331d9bbca1f981269871ab9bd79e4ec0824e
jlopezhonduras <jorge.lopezhn@gmail.com>
2025-02-12 13:49:08 -0600

    e

---
+++ datos/datos.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+var datos=[
+  {
+    "fecha": "2025-02-06",  
+    "valor": 10,
+    "fuente": "Censo de Población y Vivienda",
+    "lugar": "País",
+    "lugardet": "Honduras",
+    "periodo": 2023
+  },
+  {
+    "fecha": "2025-02-07",  
+    "valor": 25,
+    "fuente": "Censo de Población y Vivienda",
+    "lugar": "Departamento",
+    "lugardet": "Cortés",
+    "periodo": 2023
+  },
+  {
+    "fecha": "2025-02-12",  
+    "valor": 25,
+    "fuente": "Censo de Población y Vivienda",
+    "lugar": "Departamento",
+    "lugardet": "Cortés",
+    "periodo": 2023
+  }
+]
