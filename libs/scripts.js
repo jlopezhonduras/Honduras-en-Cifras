bde826e42a99cd60d78afd0eeaf898f86ff0789a
jlopezhonduras <jorge.lopezhn@gmail.com>
2025-02-12 13:37:28 -0600

    s

--- libs/scripts.js
+++ libs/scripts.js
@@ -1,16 +1,32 @@
 // JavaScript Document
 
-const urlJSON = 'https://raw.githubusercontent.com/jlopezhonduras/Honduras-en-Cifras/master/datos/datos.json';
-        
-$.get(urlJSON)
-    .done(data => {
-        $("#contenido").html(`
-            <h2>${data.pais}</h2>
-            <p>Población: ${data.poblacion.toLocaleString()}</p>
-            <p>Municipios: ${data.municipios.join(", ")}</p>
-        `);
-    })
-    .fail(error => {
-        console.error("Error:", error);
-        $("#contenido").html("Error al cargar datos.");
-    });
+$("#contenido").append("entramos"+fechaActual);
+
+$(document).ready(function() {      
+  var fechaActual = new Date().toISOString().split('T')[0];
+
+  
+  //$.getJSON('https://raw.githubusercontent.com/jlopezhonduras/Honduras-en-Cifras/master/datos/datos.json', function(datos) {
+  $.getJSON('datos/datos.jsyon', function(datos) {
+
+    var resultado = datos.find(item => item.fecha === fechaActual);
+
+    if (resultado) {
+        console.log("Datos de la fecha actual:", resultado);
+        $("#valorindicador").html(
+            resultado.valor
+            /*"Fecha: " + resultado.fecha + "<br>" +
+
+            "Fuente: " + resultado.fuente + "<br>" +
+            "Lugar: " + resultado.lugar + " (" + resultado.lugardet + ")<br>" +
+            "Periodo: " + resultado.periodo*/
+        );
+    } else {
+        console.log("No hay datos para la fecha actual.");
+        $("#resultado").html("No hay datos disponibles para hoy.");
+    }
+  }).fail(function() {
+      console.log("Error al cargar los datos.");
+      $("#valorindicador").html("Error al cargar los datos.");
+  });
+  });
